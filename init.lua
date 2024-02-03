e20f7e64a12405864d65f6a8055037ec9a695a0f
gabouriel <97214417+gabouriel@users.noreply.github.com>
2024-02-03 14:01:38 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -311,7 +311,7 @@ require('lazy').setup({
     opts.root_dir = opts.root_dir
       or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
     opts.sources = vim.list_extend(opts.sources or {}, {
-      null_ls.builtins.formatting.ocamlformat
+      null_ls.builtins.formatting.ocamlformat,
       nls.builtins.formatting.fish_indent,
       nls.builtins.diagnostics.fish,
       nls.builtins.formatting.stylua,
